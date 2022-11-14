722875f1304ac1af70bf3432cce3c0cc50ab9163
Nik Shebeda <nikshebeda@gmail.com>
2022-11-14 12:24:34 +0200

    tasks completed

---
+++ MobileCap/Extension/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constatns.swift
+//  OpenCap
+//
+//  Created by Nik on 11.11.2022.
+//
+
+import Foundation
+
+enum AlertMessagesIds: String {
+    case noInternetConnection = "1"
+    case portraitLockWarning = "2"
+}
